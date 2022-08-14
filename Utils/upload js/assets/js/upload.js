4bb56567032269c4514abae8647f92af80ed7e59
Jeremy <vaugoyeau.jeremy@gmail.com>
2022-08-14 11:57:53 +0200

    ajout progress bar

--- Utils/upload js/assets/js/upload.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('form'),
- fileInput = document.querySelector('.file-input'),
- progressArea = document.querySelector('.progress-area'), 
- uploadedArea = document.querySelector('.uploaded-area')
-
- form.addEventListener('click', ()=> {
-    fileInput.click();
- })
